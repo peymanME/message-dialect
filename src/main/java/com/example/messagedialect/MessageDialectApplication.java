1bc42d83d0500582f34a80fdea155041fd753222
peyman <peyman.ekhtiar@decsoft.com.pl>
2020-10-07 11:04:26 +0200

    Modified controller

--- src/main/java/com/example/messagedialect/MessageDialectApplication.java
+++ src/main/java/com/example/messagedialect/MessageDialectApplication.java
@@ -1,13 +1,13 @@
-/*
 package com.example.messagedialect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-*/
 /**
+ *
  * @author peyman.ekhtiar on 2020-10-07
- *//*
+ *
+*/
 
 @SpringBootApplication
 public class MessageDialectApplication {
@@ -17,4 +17,4 @@ public class MessageDialectApplication {
         System.out.println("==          Start Application         ==");
         System.out.println("========================================");
     }
-}*/
+}
